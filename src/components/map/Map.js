095c50dc01a2fb5844ffff5682a5d6226d1dff3a
Caio César Silva <caiocesarsilva08@gmail.com>
2020-05-30 09:41:40 -0300

    Map visualization

---
+++ src/components/map/Map.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+import { render } from "@testing-library/react";
+
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+    zoom: 11,
+  };
+
+  render() {
+    return (
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyDEZBGmstNOX29tWnSVv_Auy3U-mRgmAfY" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        ></GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default Map;
