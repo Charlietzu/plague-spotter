b03658710b2776478461295ea721ab968cc13823
taleslopesramos <taleslopesramos@gmail.com>
2020-05-30 12:14:13 -0300

    ADD: Data Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import MapManager from "./components/map/MapManager";
+import CountryData from "./components/map/CountryData";
+
 
 function App() {
   return (
     <div className="App">
       <MapManager />
+      <CountryData />
     </div>
   );
 }
