8e7145d0a2916e23e1d0b2b361f15a6ec401cbb0
Caio César Silva <caiocesarsilva08@gmail.com>
2020-05-31 15:20:04 -0300

    Updates

---
+++ precache-manifest.c14688ba83994351a76a0f83233a544a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcf58e752c9885322f63fba2a01e018",
+    "url": "/plague-spotter/index.html"
+  },
+  {
+    "revision": "81021a2e922cbf3c1ecd",
+    "url": "/plague-spotter/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ac631e2aac96dd7a43da",
+    "url": "/plague-spotter/static/css/main.a0c4a010.chunk.css"
+  },
+  {
+    "revision": "81021a2e922cbf3c1ecd",
+    "url": "/plague-spotter/static/js/2.098f5219.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/plague-spotter/static/js/2.098f5219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac631e2aac96dd7a43da",
+    "url": "/plague-spotter/static/js/main.c0ca76d9.chunk.js"
+  },
+  {
+    "revision": "bd38ec7111bd1e50dbee",
+    "url": "/plague-spotter/static/js/runtime-main.a002ba7a.js"
+  }
+]);
